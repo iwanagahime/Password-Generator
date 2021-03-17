03e3a933084a1f34c38733b4d98bd70f8b74f26b
iwanagahime <eliza.krucon@gmail.com>
2021-03-17 20:29:52 +0000

    add prompt for password length

--- script.js
+++ script.js
@@ -93,12 +93,44 @@ console.log(uppercaseArray);
 console.log(numberArray);
 console.log(specialCharactersArray);
 
-//
+const generatePassword = function (
+  passwordLength = prompt("What is the length of your password?")
+) // If password length is greater than or equal to 8 and password length is lesser than or equal to 128
+// Prompt returns a string value eg. 8 is actually "8"
+// Before if  cenvert string to number Number.parseInt() and store in new variable
+// If true then ask for all 4 requirements using confirm and store each in a variable
 
-const generatePassword = function () {
-  passwordLength = prompt("What is your password length?");
-};
+// if(condition){
+//   const isLowercase = confirm ("Do you want lowercase?");
+//   const isUppercase = confirm ("Do you want uppercase?");
+//   const isNumber = confirm ("Do you want number?");
+//   const isSpecialCharacter = confirm ("Do you want special character");
+// }
 
+// Start to generate password
+// const optionsArray =[
+//   ['a', 'b', 'c', 'd'],
+//   ['A', 'B', 'C', 'D']
+// ]
+
+// if(isLowercase){
+//   optionsArray.push(lowercaseArray)}
+// }
+// else if (isUppercase){
+//   optionsArray.push(uppercaseArray)
+// }
+// else if (isNumber){
+//   optionsArray.push(numberArray)
+// }
+// else {optionsArray.push(specialCharactersArray)
+// }
+
+// Get random index for options array (0,1)
+// Get the array from options array optionsArray[random index]
+// From that array get another random number between 0 and the length of that array
+// Get the random character from that array
+
+{};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
